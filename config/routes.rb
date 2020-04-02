67e46a5b0d6c9138ccb88d9b8c5ac5ab5884ae9e
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-04-02 10:49:13 -0700

    Session controller added to manage logged in users

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,14 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  # Registration Routes
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+  # Login Routes
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
